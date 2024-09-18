0df0d30d26e20cba001a5ca0da22c1df08ec2347
Max Malynowsky <github@maxmalynowsky.com>
2024-09-18 21:10:57 +0200

    initial port of jinja2 to svelte

---
+++ src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { autoType, csvParse } from 'd3-dsv';
+
+export async function getJSON(file: string) {
+  const res = await fetch(`https://cod-data.fieldmaps.io/${file}.json`);
+  const data = await res.json();
+  return data;
+}
+
+export async function getCSV(file: string) {
+  const res = await fetch(`https://cod-data.fieldmaps.io/${file}.csv`);
+  const text = await res.text();
+  return csvParse(text, autoType);
+}
