99389a31414e9632c3ba89ac55842f78405b416f
Max Malynowsky <github@maxmalynowsky.com>
2024-10-16 08:13:21 +0200

    add name checks with language

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
+rclone sync --exclude=".*" --progress --s3-no-check-bucket --s3-chunk-size=256M data/attributes r2://fieldmaps-data-cod/attributes
 rclone sync --exclude=".*" --progress --s3-no-check-bucket --s3-chunk-size=256M data/boundaries r2://fieldmaps-data-cod/boundaries
 rclone sync --exclude=".*" --progress --s3-no-check-bucket --s3-chunk-size=256M data/images r2://fieldmaps-data-cod/images
 rclone sync --exclude=".*" --progress --s3-no-check-bucket --s3-chunk-size=256M data/tables r2://fieldmaps-data-cod/tables
