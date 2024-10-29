99c2cc2c5823cddf5d88aaffa33bcd4258387619
Max Malynowsky <github@maxmalynowsky.com>
2024-10-29 18:30:12 +0100

    update to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,57 +1,12 @@
 <script lang="ts">
-  import { PUBLIC_HASH } from '$env/static/public';
   import 'normalize.css';
-  import { onMount } from 'svelte';
   interface Props {
     children?: import('svelte').Snippet;
   }
-
   let { children }: Props = $props();
-
-  const localStoragePassword = 'fieldmaps-password';
-
-  let password: string | null = $state(null);
-  let mounted = $state(false);
-
-  function hash(s: string | null) {
-    return s
-      ? s
-          .split('')
-          .reduce((a, b) => {
-            a = (a << 5) - a + b.charCodeAt(0);
-            return a & a;
-          }, 0)
-          .toString()
-      : '';
-  }
-
-  function onChange() {
-    localStorage.setItem(localStoragePassword, password);
-  }
-
-  onMount(() => {
-    password = localStorage.getItem(localStoragePassword);
-    mounted = true;
-  });
 </script>
 
-{#if hash(password) === PUBLIC_HASH}
-  {@render children?.()}
-{:else if mounted}
-  <form>
-    <input hidden type="text" autocomplete="username" value="" />
-    <label>
-      Password:
-      <input
-        bind:value={password}
-        oninput={onChange}
-        type="password"
-        autocomplete="current-password"
-        autofocus
-      />
-    </label>
-  </form>
-{/if}
+{@render children?.()}
 
 <style>
   :global(html, body) {
@@ -61,12 +16,5 @@
   :global(body) {
     font-family: system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
       'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
-
-  form {
-    align-items: center;
-    display: flex;
-    height: 100%;
-    justify-content: center;
   }
 </style>
